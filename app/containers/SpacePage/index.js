fd64818e95ebf2504dd832e46416ef1f0ed7af23
trongnguyen2706 <trongnguyen1997@gmail.com>
2021-05-24 20:37:09 +0700

    Update 3 pages

--- app/containers/SpacePage/index.js
+++ app/containers/SpacePage/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import ProposalList from '../../components/ProposalList/index';
